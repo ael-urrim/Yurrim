4b12446936d9ef0f880794e66b6ad8fb35851528
ael_urrim <elsamakins@gmail.com>
2023-05-27 21:07:15 +0100

    Version 1.2.0

--- src/pages/aboutUs/AboutUs.jsx
+++ src/pages/aboutUs/AboutUs.jsx
@@ -3,7 +3,111 @@ import "./aboutUs.scss";
 const AboutUs = () => {
   return (
     <div className="about-us">
-      <div className="about-us-card">About us page</div>
+      <div className="about-us-card">
+        <h2>About Yurrim</h2>
+        <hr />
+        <div className="body">
+          <p>
+            Welcome to Yurrim: Connecting the World, Owned by Rakmod
+            Technologies
+          </p><br />
+
+          <p>
+            Yurrim is a vibrant social media platform owned by Rakmod
+            Technologies, headquartered in Ibadan, Oyo State, Nigeria. Our
+            platform is dedicated to connecting individuals from all walks of
+            life, fostering meaningful interactions, and creating a sense of
+            belonging in the digital world. We understand the power of social
+            networks in bringing people closer, breaking barriers, and building
+            communities that thrive on collaboration and positive engagement.
+          </p><br />
+
+          <h3>Our Vision:</h3>
+          <p>
+            At Yurrim, our vision is to become the leading social media platform
+            that inspires and empowers individuals to connect, share, and make a
+            positive impact on the world. We are driven by the belief that
+            through open dialogue, celebrating diversity, and promoting
+            authentic connections, we can bridge gaps, cultivate understanding,
+            and nurture a global community where every voice is heard,
+            respected, and valued.
+          </p>
+
+          <h3>What Sets Us Apart:</h3>
+          <p>
+            Yurrim prioritizes user experience, privacy, and security. Our
+            platform offers a seamless and enjoyable interface, allowing users
+            to navigate effortlessly and discover content that resonates with
+            them. We are committed to safeguarding personal information, and
+            stringent measures are in place to ensure the privacy and security
+            of our users' data. Trust and transparency form the foundation of
+            our operations.
+          </p>
+
+          <h3>Community-Centric Approach:</h3>
+          <p>
+            At Yurrim, we recognize that communities are the heart and soul of
+            our platform. We provide users with a wide range of tools and
+            features to create, join, and engage with communities that align
+            with their passions, interests, and values. Whether you're an artist
+            seeking inspiration, a food enthusiast looking for recipes, or a
+            social activist wanting to make a difference, Yurrim offers a space
+            for you to connect with like-minded individuals, share ideas, and
+            collaborate on projects that matter.
+          </p>
+
+          <h3>Empowering Content Creators:</h3>
+          <p>
+            We understand the power of content creation in shaping conversations
+            and driving positive change. Yurrim is designed to support and
+            empower content creators of all kinds. Photographers, writers,
+            musicians, vloggers, and more, are provided with the necessary tools
+            and resources to showcase their talents, amplify their voices, and
+            build a loyal following. We celebrate creativity and encourage users
+            to explore their passions while connecting with an engaged audience.
+          </p>
+
+          <h3>Positive Engagement:</h3>
+          <p>
+            Positive and meaningful interactions are at the core of Yurrim. We
+            foster an environment where users engage respectfully and contribute
+            to constructive conversations. Our robust community guidelines
+            ensure that all users feel safe, valued, and heard. We actively
+            monitor and moderate content to maintain a healthy and inclusive
+            online space. Through reporting and feedback mechanisms, we empower
+            our community members to take an active role in shaping the platform
+            and upholding its values.
+          </p>
+
+          <h3>Constant Innovation:</h3>
+          <p>
+            Yurrim is committed to continuous improvement and innovation. We
+            listen closely to our users' feedback, gather insights, and
+            implement new features and enhancements to provide the best possible
+            experience. Staying at the forefront of social media trends,
+            emerging technologies, and user expectations is our goal. This
+            ensures that Yurrim remains relevant, engaging, and exciting for our
+            growing community.
+          </p>
+
+          <h3>Join Our Community:</h3>
+          <p>
+            We invite you to join our vibrant social media community at Yurrim.
+            Connect with friends, discover new interests, and engage in
+            conversations that matter. Together, let's build a positive and
+            inclusive digital space where ideas are shared, connections are
+            formed, and relationships are nurtured. Choose Yurrim today and
+            experience the power of community-driven social media like never
+            before.
+          </p>
+
+          <p>
+            Thank you for choosing Yurrim, the social media platform owned by
+            Rakmod Technologies. We look forward to connecting with you and
+            being a part of your social media journey.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/cookiePolicy/CookiePolicy.jsx
+++ src/pages/cookiePolicy/CookiePolicy.jsx
@@ -3,7 +3,104 @@ import "./cookiePolicy.scss";
 const CookiePolicy = () => {
   return (
     <div className="cookie-policy">
-      <div className="cookie-policy-card">Cookie policy page</div>
+      <div className="cookie-policy-card">
+        <h2>Cookie Policy</h2>
+        <hr />
+        <div className="body">
+          <h3>Effective Date: [Insert Date]</h3>
+          <p>
+            This Cookie Policy explains how Rakmod Technologies ("we," "us," or
+            "our") uses cookies and similar tracking technologies when you visit
+            our website [Insert Website URL] ("Website"). By accessing or using
+            our Website, you consent to the use of cookies as described in this
+            policy.
+          </p>
+          <h3>What are cookies?</h3>
+          <p>
+            Cookies are small text files that are placed on your computer or
+            mobile device when you visit a website. They are widely used to make
+            websites work more efficiently, enhance user experience, and provide
+            information to website owners.
+          </p>
+          <h3>How do we use cookies?</h3>
+          <p>We use cookies for various purposes on our Website, including:</p>
+          <div className="indent">
+            <h3>Essential Cookies:</h3>
+            <p>
+              These cookies are necessary for the operation of our Website. They
+              enable you to navigate the site and use its features, such as
+              accessing secure areas and making use of the shopping cart.
+            </p>
+            <h3>Analytical and Performance Cookies:</h3>
+            <p>
+              These cookies help us understand how visitors interact with our
+              Website by collecting information anonymously. They provide us
+              with insights into website traffic, popular pages, and other
+              analytics that help us improve the performance and functionality
+              of our Website.
+            </p>
+            <h3>Functionality Cookies:</h3>
+            <p>
+              These cookies allow our Website to remember choices you make and
+              provide enhanced, personalized features. They may also be used to
+              remember changes you make to text size, fonts, and other
+              customizable elements of our Website.
+            </p>
+            <h3>Advertising and Targeting Cookies:</h3>
+            <p>
+              These cookies are used to deliver advertisements that are relevant
+              to you and your interests. They are also used to limit the number
+              of times you see an ad and measure the effectiveness of
+              advertising campaigns. These cookies may be placed by third-party
+              advertising networks with our permission.
+            </p>
+          </div>
+          <h3>Third-Party Cookies:</h3>
+          <p>
+            We may also allow third-party service providers, such as analytics
+            companies and advertisers, to place cookies on our Website. These
+            third parties may collect information about your online activities
+            over time and across different websites when you use our Website.
+          </p>
+          <h3>Your Cookie Choices:</h3>
+          <p>
+            You can manage your cookie preferences and opt-out of certain
+            cookies by adjusting the settings in your browser. However, please
+            note that blocking or deleting cookies may impact your experience on
+            our Website and limit certain features.
+          </p>
+          <p>
+            For more information about managing cookies, visit the help or
+            settings page of your browser or refer to the following external
+            resources:
+          </p>
+          <ul>
+            <li>Google Chrome: support.google.com/chrome/answer/95647</li>
+            <li>
+              Mozilla Firefox:
+              support.mozilla.org/en-US/kb/enable-and-disable-cookies-website-preferences
+            </li>
+            <li>
+              Safari:
+              support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac
+            </li>
+            <li>
+              Microsoft Edge:
+              support.microsoft.com/en-us/topic/delete-and-manage-cookies-168dab11-0753-043d-7c16-ede5947fc64d
+            </li>
+          </ul>
+          <h3>Updates to this Cookie Policy:</h3> 
+          <p>We may update this Cookie Policy from
+          time to time to reflect changes in technology, legal requirements, or
+          our privacy practices. Any updates will be posted on this page with a
+          revised effective date. We encourage you to review this policy
+          periodically for any changes. Contact Us: If you have any questions
+          about our use of cookies or this Cookie Policy, please contact us at
+          [Insert Contact Information]. Please note that this Cookie Policy
+          should be read in conjunction with our Privacy Policy, which explains
+          how we collect, use, and protect your personal information.</p>
+        </div>
+      </div>
     </div>
   );
 };
